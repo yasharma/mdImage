ab5fdda6aec2bb9f03d1aa2d8076c5f6496d2f27
Yash Sharma <yashsharma205@gmail.com>
2017-07-27 13:35:38 +0530

    Update mdImage.js

--- mdImage.js
+++ mdImage.js
@@ -13,31 +13,34 @@
  * 
  * @return {String}   [template]
  */
-mimicTrade.directive('mdImage', [ function () {
-	return {
-		restrict: 'E',
-		template: `
-			<div ng-hide="isComplete" layout="row" layout-sm="column" layout-align="space-around">
-		     	<md-progress-circular md-diameter="30px" md-mode="indeterminate"></md-progress-circular>
-		   	</div>
-		   	<img ng-show="isComplete" ng-src="{{src}}" class="img-responsive" alt="{{src}}">
-		`,
-		scope: { src: '=' },
-		link: function (scope, ele) {
-			//process image to check error
-			var image = new Image();
-			image.setAttribute('src', scope.src); //assign image url
+(function () {
+	angular.module('mdImageFallback', [])
+		.directive('mdImage', [ function () {
+		return {
+			restrict: 'E',
+			template: `
+				<div ng-hide="isComplete" layout="row" layout-sm="column" layout-align="space-around">
+				<md-progress-circular md-diameter="30px" md-mode="indeterminate"></md-progress-circular>
+				</div>
+				<img ng-show="isComplete" ng-src="{{src}}" class="img-responsive" alt="{{src}}">
+			`,
+			scope: { src: '=' },
+			link: function (scope, ele) {
+				//process image to check error
+				var image = new Image();
+				image.setAttribute('src', scope.src); //assign image url
 
-			image.onload = function (e) {
-				scope.$apply(x => scope.isComplete = true);
-			};
-			image.onerror = function (e) {
-				scope.$apply(() => {
-					scope.isComplete = true;
-					scope.src = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMDAwIDEwMDAiPjxkZWZzPjxyYWRpYWxHcmFkaWVudCBpZD0icmFkaWFsLWdyYWRpZW50IiBjeD0iNTAwIiBjeT0iNTAwIiByPSI1MDAiIGdyYWRpZW50VW5pdHM9InVzZXJTcGFjZU9uVXNlIj48c3RvcCBvZmZzZXQ9IjAiIHN0b3AtY29sb3I9IiNkZmRmZGYiLz48c3RvcCBvZmZzZXQ9IjEiIHN0b3AtY29sb3I9IiM5OTkiLz48L3JhZGlhbEdyYWRpZW50PjwvZGVmcz48cmVjdCBmaWxsPSJ1cmwoI3JhZGlhbC1ncmFkaWVudCkiIHdpZHRoPSIxMDAwIiBoZWlnaHQ9IjEwMDAiLz48cGF0aCBmaWxsPSIjZmZmIiBkPSJNNjAxIDQxNGwwIDBWNTg2bDAgMEgzOTlsMCAwVjQxNGwwIDBINjAxWm0wLTE0SDM5OUExNCAxNCAwIDAgMCAzODUgNDE0djE3M2ExNCAxNCAwIDAgMCAxNCAxNEg2MDFBMTQgMTQgMCAwIDAgNjE1IDU4NlY0MTRhMTQgMTQgMCAwIDAtMTQtMTRoMFpNNTcyIDQ1MGEyMiAyMiAwIDEgMS0yMi0yMkEyMiAyMiAwIDAgMSA1NzIgNDUwWk01ODYgNTcySDQxNFY1NDNsNTAtODYgNTggNzJoMTRsNTAtNDN2ODZaIi8+PC9zdmc+';	
-				});
-				
-			};
-		}
-	};
-}]);
+				image.onload = function (e) {
+					scope.$apply(x => scope.isComplete = true);
+				};
+				image.onerror = function (e) {
+					scope.$apply(() => {
+						scope.isComplete = true;
+						scope.src = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMDAwIDEwMDAiPjxkZWZzPjxyYWRpYWxHcmFkaWVudCBpZD0icmFkaWFsLWdyYWRpZW50IiBjeD0iNTAwIiBjeT0iNTAwIiByPSI1MDAiIGdyYWRpZW50VW5pdHM9InVzZXJTcGFjZU9uVXNlIj48c3RvcCBvZmZzZXQ9IjAiIHN0b3AtY29sb3I9IiNkZmRmZGYiLz48c3RvcCBvZmZzZXQ9IjEiIHN0b3AtY29sb3I9IiM5OTkiLz48L3JhZGlhbEdyYWRpZW50PjwvZGVmcz48cmVjdCBmaWxsPSJ1cmwoI3JhZGlhbC1ncmFkaWVudCkiIHdpZHRoPSIxMDAwIiBoZWlnaHQ9IjEwMDAiLz48cGF0aCBmaWxsPSIjZmZmIiBkPSJNNjAxIDQxNGwwIDBWNTg2bDAgMEgzOTlsMCAwVjQxNGwwIDBINjAxWm0wLTE0SDM5OUExNCAxNCAwIDAgMCAzODUgNDE0djE3M2ExNCAxNCAwIDAgMCAxNCAxNEg2MDFBMTQgMTQgMCAwIDAgNjE1IDU4NlY0MTRhMTQgMTQgMCAwIDAtMTQtMTRoMFpNNTcyIDQ1MGEyMiAyMiAwIDEgMS0yMi0yMkEyMiAyMiAwIDAgMSA1NzIgNDUwWk01ODYgNTcySDQxNFY1NDNsNTAtODYgNTggNzJoMTRsNTAtNDN2ODZaIi8+PC9zdmc+';	
+					});
+
+				};
+			}
+		};
+	}]);
+}());
